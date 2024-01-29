3d5f356b32cdcb31f6dff096ad553774798b0dae
Ostapchenko Andrey <ostapchenko2005@gmail.com>
2024-01-29 14:43:12 +0300

    3task

---
+++ src/cli.js
@@ -0,0 +1,7 @@
+import readlineSync from 'readline-sync';
+
+export default function() {
+    console.log('Welcome to the Brain Games!');
+    const questionName = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${questionName}!`);
+} 
