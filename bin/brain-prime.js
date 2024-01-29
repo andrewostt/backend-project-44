224be83faa4593ef03000ec4c88f62d8f77d6b5f
Ostapchenko Andrey <ostapchenko2005@gmail.com>
2024-01-29 21:02:54 +0300

    9task

---
+++ bin/brain-prime.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+import welcomeUser, {questionName} from "../src/cli.js"
+import readlineSync from 'readline-sync';
+
+const primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+
+function brainPrime() {
+    welcomeUser();
+    console.log(`Answer "yes" if given number is prime. Otherwise answer "no".`);
+    for (let i = 0; i < 3; i += 1) {
+        const numQuestion = Math.round(Math.random() * 100);
+        const answer = readlineSync.question(`Question: ${numQuestion}\nYour answer: `);
+        if (primeNumbers.includes(numQuestion)) {
+            if (answer === `yes`) {
+                console.log('Correct');
+            } else {
+                return console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${questionName}!`);
+            }
+        } else {
+            if (answer === 'no') {
+                console.log('Correct!');
+            } else {
+                return console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${questionName}!`);
+            }
+        }
+    }
+    console.log(`Congratulations, ${questionName}`);
+}
+
+brainPrime();
