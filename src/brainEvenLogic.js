278160519e52beb98bf8fb044b202b5fde4ab7e0
Ostapchenko Andrey <andrewostt@gmail.com>
2024-04-16 10:28:09 +0300

    lint fix

--- src/brainEvenLogic.js
+++ src/brainEvenLogic.js
@@ -2,22 +2,22 @@ import readlineSync from 'readline-sync';
 import welcomeUser from './cli.js';
 
 export default function brainEvenLogic() {
-    const questionName = welcomeUser();
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
-    for (let i = 0; i < 3; i += 1) {
-      const numQuestion = Math.round(Math.random() * 100);
-      const answer = readlineSync.question(`Question: ${numQuestion}\nYour answer: `);
-      if (numQuestion % 2 === 0) {
-        if (answer === 'yes') {
-          console.log('Correct!');
-        } else {
-          return console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${questionName}!`);
-        }
-      } else if (answer === 'no') {
+  const questionName = welcomeUser();
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  for (let i = 0; i < 3; i += 1) {
+    const numQuestion = Math.round(Math.random() * 100);
+    const answer = readlineSync.question(`Question: ${numQuestion}\nYour answer: `);
+    if (numQuestion % 2 === 0) {
+      if (answer === 'yes') {
         console.log('Correct!');
       } else {
-        return console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${questionName}!`);
+        return console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${questionName}!`);
       }
+    } else if (answer === 'no') {
+      console.log('Correct!');
+    } else {
+      return console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${questionName}!`);
     }
-    return console.log(`Congratulations, ${questionName}!`);
+  }
+  return console.log(`Congratulations, ${questionName}!`);
 }
--- src/brainProgressionLogic.js
+++ src/brainProgressionLogic.js
@@ -6,28 +6,27 @@ function getRandomNumber(min, max) {
 }
 
 export default function brainProgressionLogic() {
-    const questionName = welcomeUser();
-    console.log('What number is missing in the progression?');
-    for (let i = 0; i < 3; i += 1) {
-      const length = Math.floor(Math.random() * 6) + 5;
-      const indexMissing = getRandomNumber(0, length - 1);
-      const start = Math.floor(Math.random() * 100);
-      const diff = Math.floor(Math.random() * 10) + 1;
-      const progression = [];
-
-      for (let y = 0; y < length; y += 1) {
-        const value = start + diff * y;
-        progression.push(value);
-      }
-      const missingChar = progression[indexMissing];
-      progression[indexMissing] = '..';
-      console.log(`Question: ${progression.join(' ')}`);
-      const answer = readlineSync.question('Your answer: ');
-      if (Number(answer) === missingChar) {
-        console.log('Correct!');
-      } else {
-        return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${missingChar}'.\nLet's try again, ${questionName}!`);
-      }
+  const questionName = welcomeUser();
+  console.log('What number is missing in the progression?');
+  for (let i = 0; i < 3; i += 1) {
+    const length = Math.floor(Math.random() * 6) + 5;
+    const indexMissing = getRandomNumber(0, length - 1);
+    const start = Math.floor(Math.random() * 100);
+    const diff = Math.floor(Math.random() * 10) + 1;
+    const progression = [];
+    for (let y = 0; y < length; y += 1) {
+      const value = start + diff * y;
+      progression.push(value);
     }
-    return console.log(`Congratulations, ${questionName}!`);
+    const missingChar = progression[indexMissing];
+    progression[indexMissing] = '..';
+    console.log(`Question: ${progression.join(' ')}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (Number(answer) === missingChar) {
+      console.log('Correct!');
+    } else {
+      return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${missingChar}'.\nLet's try again, ${questionName}!`);
+    }
+  }
+  return console.log(`Congratulations, ${questionName}!`);
 }
