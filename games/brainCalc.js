c3c0a781fb3bded3d6dd542cff6a34964a23ec22
Ostapchenko Andrey <andrewostt@gmail.com>
2024-10-31 19:24:11 +0300

    lint fixes

--- games/brainCalc.js
+++ games/brainCalc.js
@@ -1,28 +1,26 @@
-import readlineSync from 'readline-sync';
-import greetings from '../src/cli.js';
+import readlineSync from "readline-sync";
+import greetings from "../src/cli.js";
 
 export default function brainCalc() {
-  const operations = ['+', '-', '*'];
+  const operations = ["+", "-", "*"];
   const name = greetings();
-  console.log('What is the result of the expression?');
+  console.log("What is the result of the expression?");
   for (let i = 0; i < 3; i += 1) {
     const operationRandomNum = Math.round(Math.random() * 2);
     const randomNum = Math.round(Math.random() * 100);
     const secondRandomNum = Math.round(Math.random() * 100);
     const expression = `${randomNum} ${operations[operationRandomNum]} ${secondRandomNum}`;
     const question = readlineSync.question(
-      `Question: ${expression}\nYour answer: `,
+      `Question: ${expression}\nYour answer: `
     );
-    // const answer = new Function('return ' + expression)();
-    const answer = function() {
-      return + expression;
-    }
+    const { evaluate } = require("mathjs");
+    const answer = evaluate(expression);
     if (parseInt(question, 10) !== parseInt(answer, 10)) {
       return console.log(
-        `'${question}' is wrong answer ;(. Correct answer was '${answer}'.\nLet's try again, ${name}!`,
+        `'${question}' is wrong answer ;(. Correct answer was '${answer}'.\nLet's try again, ${name}!`
       );
     }
-    console.log('Correct!');
+    console.log("Correct!");
   }
   return console.log(`Congratulations, ${name}!`);
 }
